fc5709d41aed968e2f98390f90df7f102d604b05
chromasec <110331414+chromasec@users.noreply.github.com>
2025-07-15 15:33:07 +0200

    [+] Delete any mesage starting with the prefix and add purge.

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ nest_asyncio.apply()
 
 ascii = Center.XCenter(r"""              ______                   ______________
 _________________  /_____ ________________(_)_____  /
-___  __ \  __ \_  /_  __ `/_  ___/  __ \_  /_  __  / 
-__  /_/ / /_/ /  / / /_/ /_  /   / /_/ /  / / /_/ /  
-_  .___/\____//_/  \__,_/ /_/    \____//_/  \__,_/   
+___  __ \  __ \_  /_  __ `/_  ___/  __ \_  /_  __  /
+__  /_/ / /_/ /  / / /_/ /_  /   / /_/ /  / / /_/ /
+_  .___/\____//_/  \__,_/ /_/    \____//_/  \__,_/
 /_/                                                  """); colored = Colorate.Vertical(Colors.white_to_black, ascii)
 
 print(colored)
@@ -15,5 +15,14 @@ token = input(Center.XCenter((f"{C.LIGHTBLACK_EX}Insert your token here..{C.RESE
 prefix = ";"
 
 polaroid = commands.Bot(command_prefix=prefix, intents=discord.Intents.all(), self_bot=1); chroma = polaroid; nazareth = chroma
+
+@polaroid.event
+async def on_message(message):
+    if message.content.startswith(prefix):
+        await message.delete()
+
+@polaroid.command(name="purge")
+async def purge(ctx, amount: int):
+    await ctx.channel.purge(limit=amount)
 
 polaroid.run(token, bot=0)
