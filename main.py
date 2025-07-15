c219990a7e4aa4ece47b5c186261e0e09465726a
nazareth <rot@x0q.net>
2025-07-15 14:06:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -16,4 +16,32 @@ prefix = ";"
 
 polaroid = commands.Bot(command_prefix=prefix, intents=discord.Intents.all(), self_bot=1); chroma = polaroid; nazareth = chroma
 
+@polaroid.event
+async def on_ready():
+    print(colored)
+    print(f"(+) Username - {C.LIGHTBLACK_EX}{polaroid.user.name}{C.RESET} [{C.LIGHTBLACK_EX}{polaroid.user.id}{C.RESET}]")
+    print(f"{C.LIGHTBLACK_EX}chromasec{C.RESET} or {C.LIGHTBLACK_EX}yugokash{C.RESET} on discord for support")
+
+@polaroid.event
+async def on_message(message):
+    if message.content.startswith(prefix):
+        await message.delete()
+
+@nazareth.command(aliases=["dmsave", "ds"])
+async def dmscrape(ctx):
+    async for message in ctx.channel.history(limit=None, oldest_first=True): 
+        print(f"[{C.LIGHTBLACK_EX}{message.author.name}{C.RESET}] {C.LIGHTBLACK_EX}{message.content}{C.RESET}"); 
+        if message.attachments:
+            print(Center.XCenter(f"[{C.LIGHTBLACK_EX}{message.author.name}{C.RESET}] {C.LIGHTBLACK_EX}{message.attachments[0].url}{C.RESET}"))
+
+@nazareth.command(aliases=["mscrape", "ms"])
+async def memscrape(ctx):
+    users = set()
+    await ctx.message.delete()
+    async for message in ctx.channel.history(limit=1000):
+        users.add(message.author.id)
+    users = list(users)
+    for user in users:
+        print(Center.XCenter(user))
+
 polaroid.run(token, bot=0)
