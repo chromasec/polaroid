daead2a864979bf41ad473c85f4f68111fa305bd
nazareth <rot@x0q.net>
2025-07-15 13:25:13 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import discord; from discord.ext import commands; import random; from itertools import cycle; from colorama import Fore as C; import os
+import asyncio; import httpx; from tasksio import TaskPool; import string; import nest_asyncio; from pystyle import Colorate, Colors, Center
+nest_asyncio.apply()
+
+ascii = Center.XCenter(r"""              ______                   ______________
+_________________  /_____ ________________(_)_____  /
+___  __ \  __ \_  /_  __ `/_  ___/  __ \_  /_  __  / 
+__  /_/ / /_/ /  / / /_/ /_  /   / /_/ /  / / /_/ /  
+_  .___/\____//_/  \__,_/ /_/    \____//_/  \__,_/   
+/_/                                                  """); colored = Colorate.Vertical(Colors.white_to_black, ascii)
+
+print(colored)
+token = input(Center.XCenter((f"{C.LIGHTBLACK_EX}Insert your token here..{C.RESET} ")))
+prefix = ";"
+
+polaroid = commands.Bot(command_prefix=prefix, intents=discord.Intents.all(), self_bot=1); chroma = polaroid; nazareth = chroma
+
+polaroid.run(token, bot=0)
